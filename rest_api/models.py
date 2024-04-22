f681bb329083aae1ac17792cb221e37a7ac0eddb
eygul <erenygedek02@gmail.com>
2024-04-22 00:47:42 -0400

    complete CRUD functionalities with api view

---
+++ rest_api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(default='')
+
+    def __str__(self):
+        return self.title
